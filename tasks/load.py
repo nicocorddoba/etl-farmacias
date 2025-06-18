a743b784ed28fb7d4ffb0e75abbd820341238a67
Nicolas Cordoba <nico343@outlook.es>
2025-06-18 00:04:10 -0300

    changed request.post in load function

--- tasks/load.py
+++ tasks/load.py
@@ -6,7 +6,7 @@ import json
 def data_to_api(data:json, api_url:str):
     try:
         logger = get_run_logger()
-        r = requests.post(url=api_url, data=data)
+        r = requests.post(url=api_url, json=data)
         logger.info(r.content)
         logger.info(r.status_code)
     except requests.HTTPError as e:

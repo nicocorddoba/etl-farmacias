381307c4ebfb9ccd7e32aaf1ff403dd07288398a
Nicolas Cordoba <nico343@outlook.es>
2025-06-19 00:34:01 -0300

    tying ig again

--- plugins/scrap.py
+++ plugins/scrap.py
@@ -9,8 +9,8 @@ def ig_login(page: Page, context, logger):
     page.wait_for_selector('input[name="username"]')
 
     # 3. Completar los campos
-    page.fill('input[name="username"]', os.getenv("IG_USER"))
-    page.fill('input[name="password"]', os.getenv("FB_PASSWORD"))
+    page.fill('input[aria-label="username"]', os.getenv("FB_EMAIL"))
+    page.fill('input[aria-label="password"]', os.getenv("FB_PASSWORD"))
 
     # 4. Hacer clic en "Iniciar sesión"
     page.click('button[type="submit"]')
